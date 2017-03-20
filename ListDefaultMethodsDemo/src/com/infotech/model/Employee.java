f45b7cf0a5bed66d13171d0d26776b9575938216
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-20 23:04:50 +0530

    ListDefaultMethodsDemo source code

---
+++ ListDefaultMethodsDemo/src/com/infotech/model/Employee.java
@@ -0,0 +1,31 @@
+package com.infotech.model;
+
+public class Employee {
+
+	private String name;
+	private Double salary;
+	private String deptName;
+	
+	public Employee(String name, Double salary, String deptName) {
+		this.name = name;
+		this.salary = salary;
+		this.deptName = deptName;
+	}
+	public String getName() {
+		return name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", salary=" + salary + ", deptName=" + deptName + "]";
+	};
+}
